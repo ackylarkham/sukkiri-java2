e2cb329e1a9a64006a6aa5744c536d6e034589d6
Takehiko Akimoto <Takehiko.Akimoto2@ibm.com>
2024-09-18 07:59:08 +0900

    add: 練習10

---
+++ Ex10/src/Bank.java
@@ -0,0 +1,26 @@
+package jp.miyabilink.atm;
+
+/**
+ * 銀行クラス
+ * 
+ */
+
+public class Bank {
+    
+    /** 銀行支店名 */
+    String name;
+    /** 銀行支店住所 */
+    String address;
+
+    /** 口座開設を行うメソッド
+     *  @param owner 口座名義人
+     *  @param initBalance 口座開設時の残高 
+     */
+    public void addAccount(String owner, int initBalance){
+
+    }
+
+    public static void main(String[] args)  {
+        System.out.println("試験用のメインメソッドです");
+    }
+}
